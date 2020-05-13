a34bbc0ea0a5dbe51875c5f26d602d9a24c2102a
UpbeatDigital <60621163+UpbeatDigital@users.noreply.github.com>
2020-05-13 10:52:13 +0400

    initial commit

---
+++ app/src/main/java/digital/upbeat/estisharati_user/MainActivity.kt
@@ -0,0 +1,12 @@
+package digital.upbeat.estisharati_user
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
